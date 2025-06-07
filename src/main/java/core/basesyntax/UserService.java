c774ca536458b6f707c553aa538058e2db53eea7
Waldemar Wilhelm <vvvilgelm@gmail.com>
2025-06-07 15:27:44 +0200

    added fixes 5

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,17 +1,15 @@
 package core.basesyntax;
 
 public class UserService {
-    public String registerUser(String login, String password, String repeatPassword)
-            throws PasswordValidationException {
-
-        if (!PasswordValidator.validate(password, repeatPassword)) {
+    public String registerUser(User user) throws PasswordValidationException {
+        if (!PasswordValidator.validate(user.getPassword(), user.getRepeatPassword())) {
             return "Validation failed.";
         }
 
-        User user = new User(login, password, repeatPassword);
         return "User " + user.getUsername() + " was saved to database!!!";
     }
 }
+
 
 
 
