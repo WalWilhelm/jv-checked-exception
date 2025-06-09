9961d31d04687152e7530519347fb53e1876bd17
Waldemar Wilhelm <vvvilgelm@gmail.com>
2025-06-09 16:53:09 +0200

    added fixes 7

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserService();
-        User user = new User("email@email", "1234567890", "1234567890"); // пароль длиной 10 символов
+        User user = new User("email@email", "1234567890", "1234567890");
 
         String result = userService.registerUser(user);
         System.out.println(result);
